737657dc8ead9c89e22c4a4d7556a3e9f1c4fafc
rjtkumar@git <rjtkumardev@gmail.com>
2024-09-24 12:56:09 +0530

    Implement simplest spider

---
+++ chocolatescraper/chocolatescraper/spiders/chocolatespider.py
@@ -0,0 +1,23 @@
+"""Built following : Create Your First Scrapy Spider - Python Scrapy Beginner Series [Part 1] by ScrapeOps on youtube
+https://youtu.be/NkIlpHTFCIE?si=tdZ7LHLH30w_bGuz"""
+import scrapy
+
+
+class ChocolatespiderSpider(scrapy.Spider):
+    name = "chocolatespider" # name of the spider
+    allowed_domains = ["chocolate.co.uk"]
+    start_urls = ["https://www.chocolate.co.uk/collections/all"] # domain scrapy will start scraping from
+
+    # Appropriate selector for each product found using scrapy shell and browsers dev tools
+    # product.css("a.product-item-meta__title::text").get()
+    # product.css("span.price").get().replace('<span class="price">\n              <span class="visually-hidden">Sale price</span>', "").replace("</span>", "")
+    # product.css("a.product-item-meta__title").attrib["href"]
+
+    def parse(self, response):
+        products = response.css("product-item")
+        for product in products:
+            yield {
+                "name" : product.css("a.product-item-meta__title::text").get(),
+                "price" : product.css("span.price").get().replace('<span class="price">\n              <span class="visually-hidden">Sale price</span>', "").replace("</span>", ""),
+                "url" : product.css("a.product-item-meta__title").attrib["href"]
+            }
