8d5934f8ad94953138b927726f6503583f5d2019
rjtkumar@git <rjtkumardev@gmail.com>
2024-09-24 12:58:54 +0530

    Add pagination to spider

--- chocolatescraper/chocolatescraper/spiders/chocolatespider.py
+++ chocolatescraper/chocolatescraper/spiders/chocolatespider.py
@@ -20,4 +20,12 @@ class ChocolatespiderSpider(scrapy.Spider):
                 "name" : product.css("a.product-item-meta__title::text").get(),
                 "price" : product.css("span.price").get().replace('<span class="price">\n              <span class="visually-hidden">Sale price</span>', "").replace("</span>", ""),
                 "url" : product.css("a.product-item-meta__title").attrib["href"]
-            }
+            }  
+                
+        # Finding the link to the next page
+        # >>> response.css('[rel="next"]::attr(href)').get()
+        # '/collections/all?page=2'
+            next_page = response.css('[rel="next"]::attr(href)').get()
+            if next_page is not None:
+                next_page_url = "https://www.chocolate.co.uk" + next_page
+                yield response.follow(next_page_url, callback = self.parse)
