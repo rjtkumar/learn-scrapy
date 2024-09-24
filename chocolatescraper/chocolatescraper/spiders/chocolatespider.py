ac796bff49f7ce15db90ea6c279eca8ee13d6643
rjtkumar@git <rjtkumardev@gmail.com>
2024-09-24 14:42:22 +0530

    Refactor and Implement scrapy.ItemLoader

--- chocolatescraper/chocolatescraper/spiders/chocolatespider.py
+++ chocolatescraper/chocolatescraper/spiders/chocolatespider.py
@@ -2,6 +2,7 @@
 https://youtu.be/NkIlpHTFCIE?si=tdZ7LHLH30w_bGuz"""
 import scrapy
 from chocolatescraper.items import ChocolateProduct # importing the scrapy item
+from chocolatescraper.itemloaders import ChocolateProductLoader # importing the item loader
 
 class ChocolatespiderSpider(scrapy.Spider):
     name = "chocolatespider"
@@ -11,13 +12,14 @@ class ChocolatespiderSpider(scrapy.Spider):
     def parse(self, response):
         products = response.css("product-item")
         
-        # Refactoring to use scrapy.Items for better organization of code
-        product_item = ChocolateProduct()
         for product in products:
-            product_item["name"] = product.css("a.product-item-meta__title::text").get()
-            product_item["price"] = product.css("span.price").get().replace('<span class="price">\n              <span class="visually-hidden">Sale price</span>', "").replace("</span>", "")
-            product_item["url"] = product.css("a.product-item-meta__title").attrib["href"]
-            yield product_item    
+            #Refactoring our code to use the ItemLoader
+            chocolate = ChocolateProductLoader(item = ChocolateProduct(),selector=product)
+            chocolate.add_css("name", "a.product-item-meta__title::text")
+            chocolate.add_css("price", "span.price", re= '<span class="price">\n              <span class="visually-hidden">Sale price</span>(.*)</span>')
+            # Everthing will get removevd only contents at (.*) will be loaded
+            chocolate.add_css("url", "a.product-item-meta__title::attr(href)")
+            yield chocolate.load_item()
         
         # Finding the link to the next page
         # >>> response.css('[rel="next"]::attr(href)').get()
